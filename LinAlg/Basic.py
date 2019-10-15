5f73aec7d1e6cae08fcb0579723dc38c8a77dea6
kopok2 <oleszek.karol@gmail.com>
2019-10-15 20:55:21 +0000

    New maticer

--- LinAlg/Basic.py
+++ LinAlg/Basic.py
@@ -9,17 +9,20 @@ def production_estimate(technology, demand):
 
     Args;
         technology: Technology (Leontief) matrix (ndarray).
-        Demand: Demand vector (ndarray).
-
+        demand: Demand vector (ndarray).
 
     Returns:
         production estimates (ndarray).
     """
-    return np.dot(np.linalg.inv(np.identity(technology.shape[0]) - technology), demand.T)
+    return np.linalg.inv(np.identity(technology.shape[0]) - technology) @ demand
 
 
 if __name__ == '__main__':
     print("Linear Algebra for IO Analysis.")
     tech = np.array([[.1, .01, .01], [.02, .13, .2], [.05, .18, .05]])
     dem = np.array([2350, 4552, 911])
+    print(production_estimate(tech, dem))
+
+    tech = np.array([[.2, .0, .2], [.375, .0, .125], [.0, .8, .0]])
+    dem = np.array([50, 50, 50])
     print(production_estimate(tech, dem))
